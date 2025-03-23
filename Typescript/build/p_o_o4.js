7785757ca7d519a65b6af1b46c4e358ccf654efc
italo_leonardo <italoleonardo69@gmail.com>
2025-03-23 09:15:51 -0300

    add conteudo novo

--- Typescript/build/p_o_o4.js
+++ Typescript/build/p_o_o4.js
@@ -2,12 +2,32 @@
 class Conta {
     numero;
     titular;
+    saldoConta;
     constructor(titular) {
         this.numero = this.GerarNumeroConta();
         this.titular = titular;
+        this.saldoConta = 0;
     }
     GerarNumeroConta() {
         return Math.floor(Math.random() * 1000) + 1;
+    }
+    info() {
+        console.log(`Titular: ${this.titular}`);
+        console.log(`Número: ${this.numero}`);
+    }
+    saldo() {
+        return this.saldoConta;
+    }
+    depositar(valor) {
+        this.saldoConta += valor;
+    }
+    sacar(valor) {
+        if (valor <= this.saldoConta) {
+            this.saldoConta -= valor;
+        }
+        else {
+            console.log('Saldo insuficiente');
+        }
     }
 }
 class ContaPF extends Conta {
@@ -16,6 +36,21 @@ class ContaPF extends Conta {
         super(titular);
         this.cpf = cpf;
     }
+    ;
+    info() {
+        console.log('Conta Pessoa Física');
+        super.info();
+        console.log(`CPF: ${this.cpf}`);
+        console.log('-------------------');
+    }
+    deposito(valor) {
+        if (valor > 1000) {
+            console.log('Depósito muito alto para pessoa física');
+        }
+        else {
+            super.depositar(valor);
+        }
+    }
 }
 class ContaPJ extends Conta {
     cnpj;
@@ -23,8 +58,24 @@ class ContaPJ extends Conta {
         super(titular);
         this.cnpj = cnpj;
     }
+    ;
+    info() {
+        console.log('Conta Pessoa Juruídica');
+        super.info();
+        console.log(`CNPJ: ${this.cnpj}`);
+        console.log('-------------------');
+    }
+    ;
+    deposito(valor) {
+        if (valor > 10000) {
+            console.log('Depósito muito alto para pessoa jurídica');
+        }
+        else {
+            super.depositar(valor);
+        }
+    }
 }
 const conta1 = new ContaPF(23456, 'João');
 const conta2 = new ContaPJ(54321, 'Curso em Vídeo');
-console.log(conta1);
-console.log(conta2);
+console.log(conta1.saldo());
+console.log(conta2.saldo());
--- Typescript/src/p_o_o4.ts
+++ Typescript/src/p_o_o4.ts
@@ -1,15 +1,42 @@
 class Conta{
-    public numero: number;
-    public titular: string;
+    // Public: Atributos e métodos podem ser acessados por qualquer classe
+    // Private: Atributos e métodos só podem ser acessados pela própria classe
+    // Protected: Atributos e métodos só podem ser acessados pela própria classe e por classes filhas
+    protected numero: number;
+    protected titular: string; // Atributo protegido que pode ser acessado por classes filhas
+    private saldoConta: number;
 
     constructor(titular: string){
-        this.numero = this.GerarNumeroConta();
+        this.numero = this.GerarNumeroConta(); // Atributo número é inicializado com um número de conta aleatório
         this.titular = titular;
+        this.saldoConta = 0;     
+    
     }
 
-    GerarNumeroConta(): number{
+    private GerarNumeroConta(): number{
         return Math.floor(Math.random() * 1000)+1;
-    } // Método GerarNumeroConta() que gera um número de conta aleatório
+    } // Método GerarNumeroConta() que gera um número de conta aleatório e privado
+
+    protected info(): void{
+        console.log(`Titular: ${this.titular}`);
+        console.log(`Número: ${this.numero}`);
+    } // Método info() que exibe as informações da conta
+
+    public saldo(): number{
+        return this.saldoConta;
+    } // Método saldo() que retorna o saldo da conta
+
+    protected depositar(valor: number): void{
+        this.saldoConta += valor;
+    } // Método depositar() que deposita um valor na conta
+
+    protected sacar(valor: number): void{
+        if(valor <= this.saldoConta){
+            this.saldoConta -= valor;
+        }else{
+            console.log('Saldo insuficiente');
+        }
+    } // Método sacar() que saca um valor da conta
 
 
 } // A classe Conta é um molde para criar objetos do tipo Conta
@@ -20,7 +47,24 @@ class ContaPF extends Conta{
     constructor(cpf: number, titular: string){
         super(titular);
         this.cpf = cpf;
-    }
+
+    };
+
+    info(){
+        console.log('Conta Pessoa Física');
+        super.info();
+        console.log(`CPF: ${this.cpf}`);
+        console.log('-------------------');
+    } // Método info() que exibe as informações da conta
+
+    public deposito(valor: number) {
+        if (valor > 1000) {
+            console.log('Depósito muito alto para pessoa física');
+        }
+        else {
+            super.depositar(valor);
+        }
+    } // Método deposito() que deposita um valor na conta de pessoa física
 
 } // A classe ContaPF é um molde para criar objetos do tipo ContaPF e filha da classe Conta
 
@@ -30,11 +74,35 @@ class ContaPJ extends Conta{
     constructor(cnpj: number,titular: string){
         super(titular);
         this.cnpj = cnpj;
+    };
+
+    info(){
+        console.log('Conta Pessoa Juruídica');
+        super.info();
+        console.log(`CNPJ: ${this.cnpj}`);
+        console.log('-------------------');
+    }; // Método info() que exibe as informações da conta
+
+    public deposito(valor: number) {
+        if (valor > 10000) {
+            console.log('Depósito muito alto para pessoa jurídica');
+        }
+        else {
+            super.depositar(valor);
+        } // Método deposito() que deposita um valor na conta de pessoa jurídica
     }
+
 
 } // A classe ContaPJ é um molde para criar objetos do tipo ContaPJ e filha da classe Conta
 
-const conta1 = new ContaPF(23456, 'João');
-const conta2 = new ContaPJ(54321, 'Curso em Vídeo');
-console.log(conta1);
-console.log(conta2);
+const conta1 = new ContaPF(23456, 'João'); // new ContaPF() é a instanciação da classe ContaPF
+const conta2 = new ContaPJ(54321, 'Curso em Vídeo'); // new ContaPJ() é a instanciação da classe ContaPJ
+
+// conta1.info();
+// conta2.info();
+
+// conta1.deposito(200)
+// conta2.deposito(2000)
+
+console.log(conta1.saldo());
+console.log(conta2.saldo());
